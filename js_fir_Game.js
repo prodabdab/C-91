1ca11571222ae3163bc7b2cf548ff2b8b90dbfc6
prodabdab <73546028+prodabdab@users.noreply.github.com>
2021-03-15 18:13:10 +0530

    Add files via upload

---
+++ js_fir_Game.js
@@ -0,0 +1,44 @@
+player_1name = localStorage.getItem("player_1name");
+player_2name = localStorage.getItem("player_2name");
+
+var playerscore1= 0;
+var playerscore2= 0;
+
+document.getElementById("player_1name").innerHTML=player_1name + ":";
+document.getElementById("player_2name").innerHTML=player_2name + ":";
+
+document.getElementById("scccc").innerHTML=playerscore1;
+document.getElementById("scccc1").innerHTML=playerscore2;
+
+document.getElementById("Q").innerHTML="Question turn : " + player1;
+document.getElementById("A").innerHTML="Answer turn : " + player2;
+
+function check() {
+    tword = document.getElementById("iput_of_q").value;
+
+     sword = tword.toLowerCase();
+     console.log(sword); 
+
+     chartstorege = sword.charAt(1);
+     console.log (chartstorege);
+     lenght_divide_2 = Math.floor(word.length/2);
+	charAt2 = sword.charAt(lenght_divide_2);
+	console.log(charAt2);
+
+    lenght_minus_1 = sword.length - 1; 
+    charAt3 = sword.charAt(lenght_minus_1); 
+	console.log(charAt3);
+
+    remove_charAt1 = sword.replace(chartstorege, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+	console.log(remove_charAt3);
+
+    question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Send</button>";
+    row =  question_sword + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+document.getElementById("iput_of_q").value = "";
+
+}
